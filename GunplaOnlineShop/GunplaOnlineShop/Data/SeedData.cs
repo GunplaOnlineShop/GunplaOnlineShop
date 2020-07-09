ac99b8ebd4328c2bbf65a0a4124ce1628a833d49
TiannanXu <AlexXu46871@users.noreply.github.com>
2020-07-08 23:23:00 -0600

    MainPageGalleryChange
    New section added
    Now items are collected from db

--- GunplaOnlineShop/GunplaOnlineShop/Data/SeedData.cs
+++ GunplaOnlineShop/GunplaOnlineShop/Data/SeedData.cs
@@ -115,6 +115,8 @@ namespace GunplaOnlineShop.Data
                     Price = 42.00m,
                     Description = "This HG RX-78-2 is released for 40th Anniversary of Gundam Animation, designed by famous designer Ken Okuyama.",
                     Qantity = 10,
+                    TotalSales = 100,
+                    ReleaseDate = DateTime.Parse("12/2019"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
@@ -133,6 +135,8 @@ namespace GunplaOnlineShop.Data
                     Price = 32.00m,
                     Description = "The RX-78-02 Gundam is the titular mobile suit of the Mobile Suit Gundam: The Origin manga. It was piloted by Amuro Ray during the One Year War.",
                     Qantity = 10,
+                    TotalSales = 99,
+                    ReleaseDate = DateTime.Parse("03/2020"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
@@ -151,6 +155,8 @@ namespace GunplaOnlineShop.Data
                     Price = 30.50m,
                     Description = "The ZGMF-X42S Destiny Gundam is the titular mobile suit of Mobile Suit Gundam SEED Destiny and was piloted by Shinn Asuka. A straight-built Gunpla version is piloted by Shimon Izuna in Gundam Build Fighters Try.",
                     Qantity = 10,
+                    TotalSales = 98,
+                    ReleaseDate = DateTime.Parse("05/2019"),
                     ItemCategories = new List<ItemCategory>
                     {
                         
@@ -171,6 +177,8 @@ namespace GunplaOnlineShop.Data
                     Price = 94.00m,
                     Description = "The RX-104FF Penelope is a prototype transformable Newtype-use mobile suit. It was featured in the novel Mobile Suit Gundam: Hathaway's Flash. Without its flight unit it is referred to as the RX-104 Odysseus Gundam. The unit is piloted by Lane Aime.",
                     Qantity = 10,
+                    TotalSales = 12,
+                    ReleaseDate = DateTime.Parse("10/2019"),
                     ItemCategories = new List<ItemCategory>
                     {
                         
@@ -190,6 +198,8 @@ namespace GunplaOnlineShop.Data
                     Price = 64.50m,
                     Description = "Stolen from Z.A.F.T or gifted by Lacus Clyne? Here comes a new and updated re-design of the ZGMF-X10A Freedom Gundam.",
                     Qantity = 10,
+                    TotalSales = 50,
+                    ReleaseDate = DateTime.Parse("04/2016"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
@@ -204,6 +214,8 @@ namespace GunplaOnlineShop.Data
                     Price = 145.00m,
                     Description = "The FA-010A FAZZ is a variant of the MSZ-010 ΖΖ Gundam. It was featured in Gundam Sentinel.",
                     Qantity = 10,
+                    TotalSales = 12,
+                    ReleaseDate = DateTime.Parse("02/2020"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
@@ -218,6 +230,8 @@ namespace GunplaOnlineShop.Data
                     Price = 75.00m,
                     Description = "The RX-78-02 Gundam is the titular mobile suit of the Mobile Suit Gundam: The Origin manga. It was piloted by Amuro Ray during the One Year War.",
                     Qantity = 10,
+                    TotalSales = 32,
+                    ReleaseDate = DateTime.Parse("11/2015"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
@@ -232,6 +246,8 @@ namespace GunplaOnlineShop.Data
                     Price = 129.00m,
                     Description = "The MSN-04 Sazabi is a mobile suit that appears in Mobile Suit Gundam: Char's Counterattack. It is piloted by Char Aznable.",
                     Qantity = 10,
+                    TotalSales = 80,
+                    ReleaseDate = DateTime.Parse("12/2013"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
@@ -246,6 +262,8 @@ namespace GunplaOnlineShop.Data
                     Price = 70.00m,
                     Description = "The Astray Red Frame Kai is the upgraded version of the MBF-P02 Gundam Astray Red Frame developed and piloted by Lowe Guele. It first appeared in the photo series Mobile Suit Gundam SEED VS Astray.",
                     Qantity = 10,
+                    TotalSales = 50,
+                    ReleaseDate = DateTime.Parse("02/2010"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
@@ -260,6 +278,8 @@ namespace GunplaOnlineShop.Data
                     Price = 276.50m,
                     Description = "The RX-0 Unicorn Gundam is the titular prototype mobile suit of the Mobile Suit Gundam Unicorn novel, its OVA adaptation and the television re-cut. Developed by Anaheim Electronics for the Earth Federation, it concealed a secret that could shake the future of all humanity,[1] for it was the key to opening Laplace's Box. It is piloted by Banagher Links after the head of the Vist Foundation and his father, Cardeas Vist, entrusted it to him at the dawn of the Third Neo Zeon War in U.C. 0096.",
                     Qantity = 10,
+                    TotalSales = 25,
+                    ReleaseDate = DateTime.Parse("12/2014"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
@@ -274,6 +294,8 @@ namespace GunplaOnlineShop.Data
                     Price = 345.50m,
                     Description = "The GAT-X105+AQM/E-YM1 Perfect Strike Gundam is a mobile suit first featured in the eyecatches of the Mobile Suit Gundam SEED HD Remaster, and later made its debut in episode 36 of the HD Remaster. It is the heavy armed variant of the GAT-X105 Strike Gundam, and is piloted by Mu La Flaga.",
                     Qantity = 10,
+                    TotalSales = 5,
+                    ReleaseDate = DateTime.Parse("02/2020"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
@@ -288,6 +310,8 @@ namespace GunplaOnlineShop.Data
                     Price = 249.00m,
                     Description = "The GN-001 Gundam Exia (aka Exia, Gundam Seven Swords) is a mobile suit featured in season one of Mobile Suit Gundam 00 and is piloted by Setsuna F. Seiei.",
                     Qantity = 10,
+                    TotalSales = 30,
+                    ReleaseDate = DateTime.Parse("12/2017"),
                     ItemCategories = new List<ItemCategory>
                     {
                         new ItemCategory
---
+++ GunplaOnlineShop/GunplaOnlineShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GunplaOnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GunplaOnlineShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Item> HgItems { get; set; }
+        public List<Item> MgItems { get; set; }
+        public List<Item> PgItems { get; set; }
+        public List<Item> BestSellingItems { get; set; }
+        public List<Item> NewItems { get; set; }
+
+    }
+}
