81f7d5fe794b64d31fb8fe7b5d5a41642a7062fa
ExploreNcrack <zhaozhen@ualberta.ca>
2020-07-12 13:12:29 -0600

    clean up html string

--- GunplaOnlineShop/GunplaOnlineShop/Controllers/BaseController.cs
+++ GunplaOnlineShop/GunplaOnlineShop/Controllers/BaseController.cs
@@ -25,7 +25,9 @@ namespace GunplaOnlineShop.Controllers
             {
                 if (allCategories.Any(c => c.ParentCategory == rootCategory))
                 {
-                    navigationLinksHtml += $"<li class=\"nav-item dropdown\"><a class=\"nav-link dropdown-toggle\" href=\"{ Url.Action("Category", "Collection", new { grade = rootCategory.Name.NameEncode(), series="" }) }\" id=\"nav{rootCategory.Name.NameEncode()}\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">{rootCategory.Name.ToUpper()}</a><div class=\"dropdown-menu dropdown-menu-right\" aria-labelledby=\"{rootCategory.Name.NameEncode()}\">";
+                    navigationLinksHtml += "<li class=\"nav-item dropdown\">" +
+                                             $"<a class=\"nav-link dropdown-toggle\" href=\"{ Url.Action("Category", "Collection", new { grade = rootCategory.Name.NameEncode(), series="" }) }\" id=\"nav{rootCategory.Name.NameEncode()}\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">{rootCategory.Name.ToUpper()}</a>" +
+                                             $"<div class=\"dropdown-menu dropdown-menu-right\" aria-labelledby=\"{rootCategory.Name.NameEncode()}\">";
                     foreach (var subCategory in allCategories.Where(c => c.ParentCategory == rootCategory))
                     {
                         navigationLinksHtml += $"<a class=\"dropdown-item\" href=\"{ Url.Action("Category", "Collection", new { grade=subCategory.Name.NameEncode(), series="" }) }\">{subCategory.Name.ToUpper()}</a>";
@@ -34,7 +36,9 @@ namespace GunplaOnlineShop.Controllers
                 }
                 else
                 {
-                    navigationLinksHtml += $"<li class=\"nav - item\">< a class=\"nav-link\" href=\"{ Url.Action("Category", "Collection", new { grade = rootCategory.Name.NameEncode() }) }>{rootCategory.Name.ToUpper()}</a></li>";
+                    navigationLinksHtml += "<li class=\"nav - item\">" +
+                                             $"< a class=\"nav-link\" href=\"{ Url.Action("Category", "Collection", new { grade = rootCategory.Name.NameEncode() }) }>{rootCategory.Name.ToUpper()}</a>" +
+                                           "</li>";
                 }
             }
             ViewData["NavigationLinksHtml"] = navigationLinksHtml;
