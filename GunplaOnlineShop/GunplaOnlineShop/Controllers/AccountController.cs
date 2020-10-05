d7e69d24bab1208f2ea41a131543291ddafab350
TiannanXu <AlexXu46871@users.noreply.github.com>
2020-10-04 21:49:26 -0600

    ResetPassword
    Add reset password

--- GunplaOnlineShop/GunplaOnlineShop/Controllers/AccountController.cs
+++ GunplaOnlineShop/GunplaOnlineShop/Controllers/AccountController.cs
@@ -201,7 +201,7 @@ namespace GunplaOnlineShop.Controllers
                 {
                     var token = await _userManager.GeneratePasswordResetTokenAsync(user);
                     var passwordResetLink = Url.Action("ResetPassword", "Account",
-                        new {email = model.Email,token = token},Request.Scheme);
+                        new {email = model.Email, token = token },Request.Scheme);
                     
                     //Create email message
                     var email = new MimeMessage();
@@ -227,6 +227,56 @@ namespace GunplaOnlineShop.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ResetPassword(string token, string email)
+        {
+            // If password reset token or email is null, most likely the
+            // user tried to tamper the password reset link
+            if (token == null || email == null)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid password reset token");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Find the user by email
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user != null)
+                {
+                    // reset the user password
+                    var result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
+                    if (result.Succeeded)
+                    {
+                        return View("ResetPasswordConfirmation");
+                    }
+                    // Display validation errors. For example, password reset token already
+                    // used to change the password or password complexity rules not met
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+
+                // To avoid account enumeration and brute force attacks, don't
+                // reveal that the user does not exist
+                return View("ResetPasswordConfirmation");
+            }
+            // Display validation errors if model state is not valid
+            return View(model);
+        }
+
+
+
+        [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> ConfirmEmail(string userId, string token)
         {
             if (userId == null || token == null)
